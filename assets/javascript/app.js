84c69f9ffdd894a024c3188648ebad5cd5015256
deep-research <deep-research>
2017-10-08 11:59:02 -0500

    Timer Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-    var timeLeft
-    var timerId
+
+	// Timer Variables
+    var timeLeft = 30;
+    $("#pg2 span").text(timeLeft);
+    var timerId;
+
+    // Timer Function
+	function countdown() {
+		// Timer ends
+  		if (timeLeft == 1) {
+  			timeLeft--;
+  			$("#pg2 span").text(timeLeft);
+    		gameComplete();
+    	// Countdown continues
+  		} else {
+	  		timeLeft--;
+	  		$("#pg2 span").text(timeLeft);
+	  		console.log(timeLeft);
+  		};
+	};
 
     // Hide page 2 and 3 at start
     $("#pg2, #pg3").css("display", "none");
@@ -10,20 +28,19 @@ $(document).ready(function() {
     	$("#pg1, #pg3").css("display", "none");
     	$("#pg2").css("display", "block");
 
-    // Timer Variables
-    timeLeft = 29;
-    timerId = setInterval(countdown, 1000);
-    });
+	    // Start the timer the timer Variables
+	    timerId = setInterval(countdown, 1000);
+	    });
 
-    // Go from page 2 to 3 after the game ends
+    // Game ending sequence
     var gameComplete = function() {
     	clearTimeout(timerId);
     	$("#pg1, #pg2").css("display", "none");
     	$("#pg3").css("display", "block");
 
     	// Tally and display the results
-    	countScore()
-    	printResults()
+    	countScore();
+    	printResults();
     };
 
     // Click button to go from page 2 to 3
@@ -36,44 +53,31 @@ $(document).ready(function() {
     	location.reload(true);
     });
 
-    // Timer Function
-	function countdown() {
-		// Timer ends
-  		if (timeLeft == 0) {
-  			$("#pg2 span").text("0");
-    		gameComplete();
-    	// Countdown continues
-  		} else {
-  		$("#pg2 span").text(timeLeft);
-    	timeLeft--;
-  		};
-	};
-
 	// Score counter variables
-	var correctCount = 0
-	var incorrectCount = 0
-	var unansweredCount = 0
-	var answerList = ["3", "2", "4", "1", "4", "3", "1", "3"]
+	var correctCount = 0;
+	var incorrectCount = 0;
+	var unansweredCount = 0;
+	var answerList = ["3", "2", "4", "1", "4", "3", "1", "3"];
 
 	// Determine the score
 	var countScore = function() {
 		for (var i=0; i<answerList.length; i++) {
 			var val = $("input[name=question"+(i+1)+"]:checked").val();
 			if (val === answerList[i]) {
-				correctCount++
+				correctCount++;
 			} else if (val === undefined) {
-				unansweredCount++
+				unansweredCount++;
 			} else {
-				incorrectCount++
+				incorrectCount++;
 			};
 		};
 	};
 
 	//Display the results
 	var printResults = function() {
-		$("#correctAnswer").text(correctCount)
-		$("#incorrectAnswer").text(incorrectCount)
-		$("#unanswered").text(unansweredCount)
+		$("#correctAnswer").text(correctCount);
+		$("#incorrectAnswer").text(incorrectCount);
+		$("#unanswered").text(unansweredCount);
 	};
 
 });
